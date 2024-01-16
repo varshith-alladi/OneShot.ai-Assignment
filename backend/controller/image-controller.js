69c1d8329392dc252bd5048a5bc9470312e0ac1e
varshith-alladi <varshith.alladi20@gmail.com>
2024-01-16 20:00:50 +0530

    image upload - failed attempt

--- backend/controller/image-controller.js
+++ backend/controller/image-controller.js
@@ -1,6 +1,13 @@
 
 
+const url = 'http://localhost:8000';
 
 export const uploadImage = (req, res) => {
+    if(!req.file) 
+        return res.status(404).json("File not found");
+    
+    const imageUrl = `${url}/file/${request.file.filename}`;
 
+    res.status(200).json(imageUrl);    
+    
 }
